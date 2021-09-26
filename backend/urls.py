79d78a5df11252291817f112cc0a66fd49b6b1c8
omar115 <omarhasan115@gmail.com>
2021-09-26 16:16:36 +0600

    bugs

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 
+from backend import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from .views import *
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
